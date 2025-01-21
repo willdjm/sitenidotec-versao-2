4232a988a257c2b8b699093f8becfd5ce8ae579e
Wilkinson <wilkinsondjm@gmail.com>
2025-01-21 00:00:12 -0300

    ajustes

--- src/app/components/banners.tsx
+++ src/app/components/banners.tsx
@@ -1,98 +1,100 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useMemo } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md"; // Importando os ícones do React Icons (Chevron Left e Chevron Right)
-
-const images = [
-  {
-    src: "/1.jpg",
-    text: "Texto para a Imagem 1",
-  },
-  {
-    src: "/2.jpg",
-    text: "Texto para a Imagem 2",
-  },
-  {
-    src: "/3.jpg",
-    text: "Texto para a Imagem 3",
-  },
-  {
-    src: "/4.jpg",
-    text: "Texto para a Imagem 1",
-  },
-  {
-    src: "/5.jpg",
-    text: "Texto para a Imagem 2",
-  },
-  {
-    src: "/6.jpg",
-    text: "Texto para a Imagem 3",
-  },
-];
 
 export const Banner = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [randomImage, setRandomImage] = useState<string>(images[0].src);
-  const [imageText, setImageText] = useState<string>(images[0].text);
+  const [randomVideo, setRandomVideo] = useState<string>(""); // Iniciar como uma string vazia
   const [isFading, setIsFading] = useState<boolean>(false);
 
-  // Função para mudar a imagem
-  const changeImage = (nextIndex: number) => {
-    setIsFading(true); // Inicia o fade-out da imagem atual
+  // UseMemo para memorizar o array de vídeos e evitar recriação em cada renderização
+  const videos = useMemo(() => [
+    {
+      desktop: "/video-banner-home-nidoimovel.mp4", // Vídeo para desktop na posição 1
+      mobile: "/video-banner-home-nidoimovel-mobile.mp4",   // Vídeo para mobile na posição 1
+    },
+    {
+      desktop: "/video-banner-home-nidoimovel.mp4", // Vídeo para desktop na posição 1
+      mobile: "/video-banner-home-nidoimovel-mobile.mp4",   // Vídeo para mobile na posição 1
+    },
+    {
+      desktop: "/video-banner-home-nidoimovel.mp4", // Vídeo para desktop na posição 1
+      mobile: "/video-banner-home-nidoimovel-mobile.mp4",   // Vídeo para mobile na posição 1
+    },
+  ], []); // Array de vídeos não muda, então o array de dependências é vazio
+
+  // Função para determinar qual vídeo usar dependendo do tamanho da tela (desktop ou mobile)
+  const getVideoForDevice = useCallback(() => {
+    const isMobile = window.innerWidth <= 768; // Lógica simples para detectar se é mobile (tamanho de tela <= 768px)
+    return isMobile ? videos[currentIndex].mobile : videos[currentIndex].desktop;
+  }, [currentIndex, videos]); // Agora, vídeos é uma dependência memorizada
+
+  // Função para mudar o vídeo
+  const changeVideo = useCallback((nextIndex: number) => {
+    setIsFading(true); // Inicia o fade-out do vídeo atual
     setTimeout(() => {
       setCurrentIndex(nextIndex);
-      setRandomImage(images[nextIndex].src);
-      setImageText(images[nextIndex].text);
-      setIsFading(false); // Após a transição, finaliza o fade-in da nova imagem
+      setRandomVideo(getVideoForDevice()); // Atualiza o vídeo conforme o tipo de dispositivo
+      setIsFading(false); // Após a transição, finaliza o fade-in do novo vídeo
     }, 500); // Espera o tempo da transição (500ms)
-  };
+  }, [getVideoForDevice]); // Aqui a dependência é getVideoForDevice
 
-  // Efeito para trocar a imagem automaticamente a cada 3 segundos
+  // Efeito para trocar o vídeo automaticamente a cada 5 segundos
   useEffect(() => {
+    setRandomVideo(getVideoForDevice()); // Define o vídeo inicial dependendo do dispositivo
     const intervalId = setInterval(() => {
-      const nextIndex = (currentIndex + 1) % images.length;
-      changeImage(nextIndex);
+      const nextIndex = (currentIndex + 1) % videos.length;
+      changeVideo(nextIndex);
     }, 5000);
 
     return () => clearInterval(intervalId); // Limpeza do intervalo quando o componente for desmontado
-  }, [currentIndex]);
+  }, [currentIndex, changeVideo, getVideoForDevice, videos.length]); // Aqui também, vídeos.length é necessário
 
-  // Função para navegação manual (próxima imagem)
-  const goToNextImage = () => {
-    const nextIndex = (currentIndex + 1) % images.length;
-    changeImage(nextIndex);
+  // Função para navegação manual (próximo vídeo)
+  const goToNextVideo = () => {
+    const nextIndex = (currentIndex + 1) % videos.length;
+    changeVideo(nextIndex);
   };
 
-  // Função para navegação manual (imagem anterior)
-  const goToPrevImage = () => {
-    const prevIndex = (currentIndex - 1 + images.length) % images.length;
-    changeImage(prevIndex);
+  // Função para navegação manual (vídeo anterior)
+  const goToPrevVideo = () => {
+    const prevIndex = (currentIndex - 1 + videos.length) % videos.length;
+    changeVideo(prevIndex);
   };
 
   return (
-    <div className="relative w-full h-[30rem] overflow-hidden shadow-lg"> {/* Sombra adicionada aqui */}
-      {/* Imagem com transição Fade-in */}
+    <div className="relative w-full h-80 lg:h-[30rem] overflow-hidden shadow-lg">
+      {/* Vídeo com transição Fade-in */}
       <div
-        className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-500 ease-in-out ${
+        className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-in-out ${
           isFading ? "opacity-0" : "opacity-100"
         }`}
-        style={{
-          backgroundImage: `url(${randomImage})`,
-        }}
-      />
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-10 flex items-center justify-center">
-        <h2 className="text-white text-3xl font-bold">{imageText}</h2>
+      >
+        {/* Renderizar o vídeo apenas se randomVideo não estiver vazio */}
+        {randomVideo && (
+          <video
+            key={randomVideo} // Garantir que o vídeo seja atualizado quando o src mudar
+            className="w-full h-full object-cover"
+            autoPlay
+            muted
+            loop
+          >
+            <source src={randomVideo} type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+        )}
       </div>
 
       {/* Setas de navegação (visíveis apenas em telas grandes) */}
       <button
-        onClick={goToPrevImage}
+        onClick={goToPrevVideo}
         className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-sky-500 bg-opacity-75 text-white p-3 rounded-full shadow-lg hover:bg-opacity-75 focus:outline-none hidden md:block transition-all duration-300 ease-in-out hover:scale-110"
       >
         <MdChevronLeft className="h-6 w-6" />
       </button>
       <button
-        onClick={goToNextImage}
+        onClick={goToNextVideo}
         className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-sky-500 bg-opacity-75 text-white p-3 rounded-full shadow-lg hover:bg-opacity-75 focus:outline-none hidden md:block transition-all duration-300 ease-in-out hover:scale-110"
       >
         <MdChevronRight className="h-6 w-6" />
