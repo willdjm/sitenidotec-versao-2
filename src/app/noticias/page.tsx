390034b7e605ff461d3a5df6b25099be1c073aa4
Wilkinson <wilkinsondjm@gmail.com>
2025-04-20 00:22:23 -0300

    ajustes

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,4 +1,3 @@
-import { BannerPages } from "../components/bannersPages";
 import { FloatingButtons } from "../components/FloatingButtons";
 import { Footer } from "../components/footer";
 import { Navbar } from "../components/navbar";
@@ -8,7 +7,6 @@ export default function Noticias() {
   return (
     <main className="bg-zinc-50">
       <Navbar />
-      <BannerPages mobileImage="/banner-noticias-mobile.png" tabletImage="/banner-noticias.png" desktopImage="/banner-noticias.png" />
 
       <NoticesList />
       <Footer />
